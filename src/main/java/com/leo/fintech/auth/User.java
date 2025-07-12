7e0e928f9aeb6c212dab438538d94587d0873921
leocesar3dofficial <leocesarbsb@gmail.com>
2025-07-12 03:34:56 -0300

    feat: Update User entity to use UUID for id and modify database schema for users and related tables

--- src/main/java/com/leo/fintech/auth/User.java
+++ src/main/java/com/leo/fintech/auth/User.java
@@ -1,9 +1,10 @@
 package com.leo.fintech.auth;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -24,8 +25,9 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    @Column(columnDefinition = "UUID")
+    private UUID id;
 
     @Column(nullable = false, length = 255)
     private String email;
