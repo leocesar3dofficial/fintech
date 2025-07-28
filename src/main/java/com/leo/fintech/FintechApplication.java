3d1bac7865f36ccd1b0b49cd4606e9e7a95ca09b
leocesar3dofficial <leocesarbsb@gmail.com>
2025-07-28 00:16:13 -0300

    refactor: OpenAPIDefinition annotation

--- src/main/java/com/leo/fintech/FintechApplication.java
+++ src/main/java/com/leo/fintech/FintechApplication.java
@@ -3,6 +3,16 @@ package com.leo.fintech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Fintech API",
+        version = "1.0",
+        description = "API documentation for Fintech application"
+    )
+)
 @SpringBootApplication
 public class FintechApplication {
 
