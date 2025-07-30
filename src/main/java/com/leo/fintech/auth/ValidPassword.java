d8c7b7dbb393b3beab9f0e0bd828880520c40135
leocesar3dofficial <leocesarbsb@gmail.com>
2025-07-30 04:31:33 -0300

    formatação

--- src/main/java/com/leo/fintech/auth/ValidPassword.java
+++ src/main/java/com/leo/fintech/auth/ValidPassword.java
@@ -1,15 +1,20 @@
 package com.leo.fintech.auth;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
-import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = PasswordValidator.class)
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
+
     String message() default "Invalid password";
 
     int minLength() default 8;
@@ -23,4 +28,5 @@ public @interface ValidPassword {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
 }
